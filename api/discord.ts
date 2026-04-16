975bf66e953278452761b49282c9dab5ce38edd1
report11071216-ux <report11071216@gmail.com>
2026-04-16 16:58:57 +0900

    Update discord.ts

--- api/discord.ts
+++ api/discord.ts
@@ -1,1 +1,15 @@
+export default async function handler(req, res) {
+  const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 
+  await fetch(webhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: req.body.message,
+    }),
+  });
+
+  res.status(200).json({ ok: true });
+}
